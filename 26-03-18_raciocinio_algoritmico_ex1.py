38528317cca0f186e92ee50058513572dfa0d222
kaue ogibowski <kaueogibowski@outlook.com>
2026-03-18 19:22:23 -0300

    first commit

---
+++ 26-03-18_raciocinio_algoritmico_ex1.py
@@ -0,0 +1,30 @@
+# idade = int(input("Digite a sua idade: "))
+# print(idade)
+# if idade >= 18 :
+#     print("Você é maior de idade")
+# else :
+#     print("Você é menor de idade")
+
+#-----------------------------------------#
+
+# numero1 = int(input("Digite o primeiro número: "))
+# numero2 = int(input("Digite o segundo número: "))
+
+# soma = numero1+numero2
+# sub = numero1-numero2
+# div = numero1/numero2
+# mult = numero1 * numero2
+
+# print("A soma dos números é: ", soma)
+# print("a subtração dos números é: ", sub)
+# print("a divisão dos números é: ", div)
+# print("a multiplicação dos números é: ", mult)
+
+#-----------------------------------------#
+
+# idade = int(input("Digite a sua idade: "))
+# print("Sua idade é: ", idade)
+
+#-----------------------------------------#
+
+# print("Olá mundo")
---
+++ 26-03-18_raciocinio_algoritmico_ex2.py
@@ -0,0 +1,48 @@
+# nota1 = float(input("Digite a primeira nota: "))
+# nota2 = float(input("Digite a segunda nota: "))
+# média = (nota1 + nota2)/ 2
+
+# print("A média é: ", média)
+
+#-----------------------------------------#
+
+# preco = float(input("Digite o valor do produto: "))
+# desconto = float(input("Digite a porcentagem do desconto: "))
+# valor_final = preco - (preco * desconto / 100)
+
+
+# print("O valor com desconto é: ", valor_final)
+
+#-----------------------------------------#
+
+# numero = int(input("Digite um número inteiro: "))
+
+# tester = numero % 2
+# # print(tester)
+
+# if tester == 1 :
+#     print("Número impar")
+# else :
+#     print("Número par")
+
+#-----------------------------------------#
+
+# Ex. 4 - b
+
+#-----------------------------------------#
+
+# Ex. 5 - d
+
+#-----------------------------------------#
+
+# Ex. 6 - a
+
+#-----------------------------------------#
+
+# Ex. 7 - a
+
+#-----------------------------------------#
+
+# Ex. 8 - a
+
+#-----------------------------------------#
